d4222dd8dfce8672445d7789b1fb1b34917ad796
tylerhaw1horne-pixel <tylerhaw1horne@gmail.com>
2026-03-01 21:52:35 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,28 @@
-const CACHE_NAME="huey-kitchen-v2";
+const CACHE_NAME = "hueys-recipes-v1";
 
-const urlsToCache=[
-"./",
-"./index.html",
-"./items_sides.json",
-"./sauces_spices.json",
-"./dressings.json",
-"./food_prepped.json",
-"./desserts.json",
-"./brunch.json"
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./items_sides.json",
+  "./sauces_spices.json",
+  "./dressings.json",
+  "./food_prepped.json",
+  "./desserts.json",
+  "./brunch.json"
 ];
 
-self.addEventListener("install",event=>{
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache=>cache.addAll(urlsToCache))
-);
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>{
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener("fetch",event=>{
-event.respondWith(
-caches.match(event.request)
-.then(response=>response||fetch(event.request))
-);
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(response=>{
+      return response || fetch(e.request);
+    })
+  );
 });
