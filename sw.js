5e02505e4ef50dc69a9fc54483d1e52b53e2a9b8
tylerhaw1horne-pixel <tylerhaw1horne@gmail.com>
2026-03-01 23:02:10 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,26 @@
-const CACHE_NAME = "hueys-recipes-v2"; // Change version when deploying
+const CACHE_NAME = "hueys-v2";
 
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./items_sides.json",
-  "./sauces_spices.json",
-  "./dressings.json",
-  "./food_prepped.json",
-  "./desserts.json",
-  "./brunch.json"
-];
-
-// Install
-self.addEventListener("install", event => {
+self.addEventListener("install", e=>{
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
 });
 
-// Activate & clear old caches
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
-    )
+self.addEventListener("activate", e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>{
+      return Promise.all(keys.map(key=>{
+        if(key !== CACHE_NAME){
+          return caches.delete(key);
+        }
+      }));
+    })
   );
   self.clients.claim();
 });
 
-// Network-first strategy (always try fresh first)
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      })
-      .catch(() => caches.match(event.request))
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    fetch(e.request).catch(()=>{
+      return caches.match(e.request);
+    })
   );
 });
