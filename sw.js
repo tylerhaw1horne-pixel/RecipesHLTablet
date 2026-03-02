7d8d5cf9d4a6e567b21407429f1374539faceba1
tylerhaw1horne-pixel <tylerhaw1horne@gmail.com>
2026-03-01 21:34:38 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,26 @@
-const CACHE_NAME = "huey-kitchen-v1";
+const CACHE_NAME="huey-kitchen-v2";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./recipes.json"
+const urlsToCache=[
+"./",
+"./index.html",
+"./items_sides.json",
+"./sauces_spices.json",
+"./dressings.json",
+"./food_prepped.json",
+"./desserts.json",
+"./brunch.json"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install",event=>{
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache=>cache.addAll(urlsToCache))
+);
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener("fetch",event=>{
+event.respondWith(
+caches.match(event.request)
+.then(response=>response||fetch(event.request))
+);
 });
